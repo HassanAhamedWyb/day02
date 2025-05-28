b905d44c29dc1fe3b5d22a7a9f98f1ad560e1acb
Geshan Anupama <129172291+Geshananupama@users.noreply.github.com>
2025-05-28 19:02:52 +0530

    add

---
+++ example2.py
@@ -0,0 +1,9 @@
+def BirthdayWish (name,age):
+    
+    print(f"Happy birthday {name} {age}")
+    
+    print("Happy birthday",name,age)
+    print("You are now old",name)
+
+BirthdayWish ("fahath",22) 
+BirthdayWish (" maha",25)  
